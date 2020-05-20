a8fe75d309dc211c84a115e3825acaa3056e5670
Tagai Nao <tagainao@icloud.com>
2020-05-20 16:03:09 +0900

    field作成＆床との当たり判定
    5×5×10のfieldをスクリプトからつくれるようにした。
    床とピースが接触したときにピースが静止するようになった。

---
+++ 3DTetris/Assets/script/hit.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        //move_flag = true;
+        Debug.Log(collision.gameObject.name + "Enter"); // ログを表示する
+    }
+    void OnCollisionStay(Collision collision)
+    {
+        //move_flag = true;
+        Debug.Log(collision.gameObject.name + "Enter"); // ログを表示する
+    }
+    void OnCollisionExit(Collision collision)
+    {
+        //move_flag = true;
+        Debug.Log(collision.gameObject.name + "Enter"); // ログを表示する
+    }
+
+}
--- 3DTetris/Assets/script/move_peace.cs
+++ 3DTetris/Assets/script/move_peace.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class move_peace : MonoBehaviour
 {
     private float timeElapsed;
+    private bool move_flag = false;
 
     // Start is called before the first frame update
     void Start()
@@ -17,10 +18,27 @@ public class move_peace : MonoBehaviour
     void Update()
     {
         timeElapsed += Time.deltaTime;
-        if (timeElapsed >= 2)
+        if (timeElapsed >= 2 && move_flag == false)
         {
             this.gameObject.transform.Translate(Vector3.down);
             timeElapsed = 0.0f;
         }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        move_flag = true;
+        this.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+        Debug.Log(collision.gameObject.name + "Enter"); // ログを表示する
+    }
+    void OnCollisionStay(Collision collision)
+    {
+        //move_flag = true;
+        //Debug.Log(collision.gameObject.name + "Stay"); // ログを表示する
+    }
+    void OnCollisionExit(Collision collision)
+    {
+        //move_flag = true;
+        //Debug.Log(collision.gameObject.name + "Exit"); // ログを表示する
     }
 }
