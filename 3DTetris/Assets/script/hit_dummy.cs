236bfef686f27a76419a713c2c574c727f12214e
Tagai Nao <tagainao@icloud.com>
2020-05-28 02:22:27 +0900

    キューブを置く処理を変更
    キューブを置くときに、移動先の当たり判定を考えておくようにした。

---
+++ 3DTetris/Assets/script/hit_dummy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hit_dummy : MonoBehaviour
+{
+    public GameObject piece_script;
+    public GameObject field_obj;
+
+    private field field_script;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log(this.gameObject.tag);
+        field_script = field_obj.GetComponent<field>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (gameObject.tag == "floor")
+        {
+            if (collider.gameObject.tag == "dummy")
+            {
+                field_script.set_cube(GameObject.Find("peace"));
+            }
+        }
+
+    }
+}
