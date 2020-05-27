e01a2dbee1979d135d96fcb1fbf27b2837bb3285
shou10608 <shou10608@users.noreply.github.com>
2020-05-27 15:02:49 +0900

    create_piece

---
+++ 3DTetris/Assets/script/create_piece.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class create_piece: MonoBehaviour
+{
+
+    public GameObject JminoPrefab;
+    public GameObject JminoObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+}
