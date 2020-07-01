2dfc528217148635c56a4ae77ce74e16af98ed7c
Tagai Nao <tagainao@icloud.com>
2020-07-01 21:58:57 +0900

    タイトル画面追加

---
+++ 3DTetris/Assets/script/minorotate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class minorotate : MonoBehaviour
+{
+    private Vector3 _Rotation;
+    public float speed = 5.0f;
+    public float rotAngle = 360f;
+    private float variation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        variation = rotAngle / speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _Rotation = gameObject.transform.localEulerAngles;
+        Debug.Log(_Rotation);
+        gameObject.transform.localEulerAngles += new Vector3 (0.0f, variation * Time.deltaTime, 0.0f);
+    }
+}
