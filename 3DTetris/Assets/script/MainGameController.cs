2c855af7689f971d3d926a7336a64b7c3c04a216
Tagai Nao <tagainao@icloud.com>
2020-10-07 14:25:39 +0900

    選択したステージ情報を保持して移動できるように変更

---
+++ 3DTetris/Assets/script/MainGameController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainGameController : MonoBehaviour
+{
+    public static int[,] stagedata;
+
+    // setter
+    public static void setStageData(int[,] data)
+    {
+        stagedata = data;
+    }
+
+    // getter
+    public static int[,] getStageData()
+    {
+        return stagedata;
+    }
+
+
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+    }
+}
--- 3DTetris/Assets/script/field.cs
+++ 3DTetris/Assets/script/field.cs
@@ -52,20 +52,23 @@ public class field : MonoBehaviour
     {
         count_bottom = 0;
         bottom = new int[field_x+2, field_z+2];
-        for(var w = 0; w < field_x+2; w++)
-        {
-            for(var h = 0; h < field_z+2; h++)
-            {
-                if (((3 <= w && w <= 5) || (3 <= h && h <= 5)) && (w != 0 && w != field_x + 1 && h != 0 && h != field_z + 1))  
-                {
-                    bottom[w, h] = _NON;
-                    count_bottom++;
-                }
-                else
-                    bottom[w, h] = _KABE;
-            }
-        }
-        
+        //for(var w = 0; w < field_x+2; w++)
+        //{
+        //    for(var h = 0; h < field_z+2; h++)
+        //    {
+        //        if (((3 <= w && w <= 5) || (3 <= h && h <= 5)) && (w != 0 && w != field_x + 1 && h != 0 && h != field_z + 1))  
+        //        {
+        //            bottom[w, h] = _NON;
+        //            count_bottom++;
+        //        }
+        //        else
+        //            bottom[w, h] = _KABE;
+        //    }
+        //}
+        //bottom = MainGameController.getStageData();
+        Debug.Log(MainGameController.stagedata);
+        bottom = MainGameController.stagedata;
+
         /* --------------------------------------- *
          *         fieldを配列で生成               *
          *                                         *
@@ -101,6 +104,7 @@ public class field : MonoBehaviour
                     else
                     {
                         field_array[z, y, x] = _NON;
+                        count_bottom++;
                     }
                 }
             }
